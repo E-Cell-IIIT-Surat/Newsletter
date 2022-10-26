e3579f4eabbda65a2c395525cfe4e850224b6ff6
Har-sh-arma <harinathsharma303@gmail.com>
2022-10-26 17:05:29 +0530

    Added Signup Functionality, OTP based Email verification

---
+++ action/email_verification.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const SENDER_PASS = process.env.SENDERPASS;
+const nodemailer = require('nodemailer');
+
+const get_OTP = () =>{
+	let otp = Math.ceil(((Math.random()*10000)+1000)%9999);
+	return otp;
+}
+
+const send_mail = async (usr_email, otp) => {
+  const transporter= nodemailer.createTransport({
+      service : "Gmail",
+      auth :{
+          user:"harsharmassignment@gmail.com",
+          pass:SENDER_PASS
+      }
+  })
+  const options={
+      from :"harsharmassignment@gmail.com",
+      to:`${usr_email}`,
+      subject:"OTP FOR VERIFICATION",
+      text: `${otp}`,
+    }
+ 
+  transporter.sendMail(options, function(err,info){
+      if(err){
+          console.log(err);
+          return;
+      }
+      console.log("Sent" +info.response)
+  })
+}
+
+module.exports = {get_OTP, send_mail};
